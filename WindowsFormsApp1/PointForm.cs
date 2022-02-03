840e5de1df03cc86fb85832bd5152bfb0961c5ee
Stepan <pirogov1923@mail.ru>
2022-02-03 18:45:56 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/PointForm.cs
@@ -0,0 +1,148 @@
+﻿using Newtonsoft.Json;
+using PointLib;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace WindowsFormsApp1
+{
+    public partial class PointForm : Form
+    {
+        private Point[] points = null;
+        int flag = 0;
+        public PointForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, System.EventArgs e)
+        {
+            points = new Point[5];
+            flag = 1;
+            var rnd = new Random();
+
+            for (int i = 0; i < points.Length; i++)
+                points[i] = rnd.Next(3) % 2 == 0 ? new Point() : new Point3D();
+
+            listBox.DataSource = points;
+
+        }
+
+        private void btnSort_Click(object sender, System.EventArgs e)
+        {
+            if (points == null)
+                return;
+
+            Array.Sort(points);
+
+            listBox.DataSource = null;
+            listBox.DataSource = points;
+
+        }
+
+        private void btnSerialize_Click(object sender, EventArgs e)
+        {
+            var dlg = new SaveFileDialog();
+            dlg.Filter = "SOAP|*.soap|XML|*.xml|JSON|*.json|Binary|*.bin";
+
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            using (var fs =
+                new FileStream(dlg.FileName, FileMode.Create, FileAccess.Write))
+            {
+                switch (Path.GetExtension(dlg.FileName))
+                {
+                    case ".bin":
+                        var bf = new BinaryFormatter();
+                        bf.Serialize(fs, points);
+                        break;
+                    case ".soap":
+                        var sf = new SoapFormatter();
+                        sf.Serialize(fs, points);
+                        break;
+                    case ".xml":
+                        var xf = new XmlSerializer(typeof(Point[]), new[] { typeof(Point3D) });
+                        xf.Serialize(fs, points);
+                        break;
+                    case ".json":
+                        var jf = new JsonSerializer();
+                        using (var w = new StreamWriter(fs))
+                            jf.Serialize(w, points);
+                        break;
+
+                }
+            }
+
+
+        }
+
+        private void btnDeserialize_Click(object sender, EventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            dlg.Filter = "SOAP|*.soap|XML|*.xml|JSON|*.json|Binary|*.bin";
+
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            using (var fs =
+                new FileStream(dlg.FileName, FileMode.Open, FileAccess.Read))
+            {
+                switch (Path.GetExtension(dlg.FileName))
+                {
+                    case ".bin":
+                        var bf = new BinaryFormatter();
+                        points = (Point[])bf.Deserialize(fs);
+                        break;
+                    case ".soap":
+                        var sf = new SoapFormatter();
+                        points = (Point[])sf.Deserialize(fs);
+                        break;
+                    case ".xml":
+                        var xf = new XmlSerializer(typeof(Point[]), new[] { typeof(Point3D) });
+                        points = (Point[])xf.Deserialize(fs);
+                        break;
+                    case ".json":
+                        var jf = new JsonSerializer();
+                        using (var r = new StreamReader(fs))
+                        {
+                            dynamic result = JsonConvert.DeserializeObject(r.ReadToEnd());
+
+                            if (flag == 0)
+                            {
+                                points = new Point[5];
+                            }
+                            Array.Clear(points, 0, 5);
+                            for (int i = 0; i < 5; i++)
+                            {
+                                if (result[i].Z != null)
+                                {
+                                    Point3D a = new Point3D();
+                                    a.X = result[i].X;
+                                    a.Y = result[i].Y;
+                                    a.Z = result[i].Z;
+                                    points[i] = a;
+                                }
+                                else
+                                {
+                                    Point b = new Point();
+                                    b.X = result[i].X;
+                                    b.Y = result[i].Y;
+                                    points[i] = b;
+                                }
+                            }
+                        }
+                        break;
+                }
+            }
+
+            listBox.DataSource = null;
+            listBox.DataSource = points;
+
+
+        }
+    }
+}
